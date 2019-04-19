54ca64719eb24c70e9cbfa71f4439d510608bc28
docker <anthonylon@gmail.com>
2019-04-18 22:43:06 -0600

    new file:   dockerinstall.sh

---
+++ dockerinstall.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
+echo 'deb [arch=amd64] https://download.docker.com/linux/debian buster stable' > /etc/apt/sources.list.d/docker.list
+apt-get update
+apt-get -y install docker-ce
+docker run hello-world
