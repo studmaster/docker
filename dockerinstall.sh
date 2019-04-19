bbfa2d3ff4d3ce87e852a85ffa21cd6ab82c651a
studmaster <anthonylon@gmail.com>
2019-04-18 23:08:15 -0600

    Update dockerinstall.sh
    added code to get curl.

--- dockerinstall.sh
+++ dockerinstall.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+apt update
+apt -y install curl
 curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
 echo 'deb [arch=amd64] https://download.docker.com/linux/debian buster stable' > /etc/apt/sources.list.d/docker.list
 apt-get update
